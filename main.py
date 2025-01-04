0d56a9847d7b5c11433e34a1e359aca5df8f2657
Dandrik <nelson.shane@gmail.com>
2025-01-04 12:37:35 -0800

    Inital git commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+#import os
+#os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
+import pygame
+
+from constants import *
+
+def main():
+#  print("Starting asteroids!")
+#  print("Screen width:",SCREEN_WIDTH)
+#  print("Screen height:",SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+
+         for event in pygame.event.get():
+             if event.type == pygame.QUIT:
+                 return
+         background_color = (0, 0, 0)
+         screen.fill(background_color)
+         pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
