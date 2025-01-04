2683636bd34af88905e30f56de0ea6f0d191fabe
Dandrik <nelson.shane@gmail.com>
2025-01-04 14:07:30 -0800

    test

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
 
          for event in pygame.event.get():
@@ -23,6 +26,7 @@ def main():
          screen.fill(background_color)
          pygame.display.flip()
 
+         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
